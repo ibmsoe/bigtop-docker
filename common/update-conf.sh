9bcd4fba847150d00c542b0115fb6533ecab1bd0
Amir Sanjar <amir.sanjar@canonical.com>
2016-05-04 19:14:01 -0500

    add zeppelin

--- common/update-conf.sh
+++ common/update-conf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+NAMENODE=$1
+RESOURCEMANAGER=$2
 add_element(){
   name=$1
   value=$2
@@ -10,11 +11,11 @@ add_element(){
   sed -i -e "/<\/configuration>/ s/.*/${C}\n&/" $xml_file
 }
 ## Add and init yarn.resourcemanager.address in yarn-site.xml
-sed -i s/localhost/namenode/ /etc/hadoop/conf/core-site.xml
-sed -i s/localhost/namenode/ /etc/hadoop/conf/mapred-site.xml
-add_element "yarn.resourcemanager.hostname" "namenode" "/etc/hadoop/conf/yarn-site.xml"
-add_element "yarn.resourcemanager.address" "namenode:8032" "/etc/hadoop/conf/yarn-site.xml"
-add_element "yarn.resourcemanager.resource-tracker.address" "namenode:8031" "/etc/hadoop/conf/yarn-site.xml"
-add_element "yarn.resourcemanager.scheduler.address" "namenode:8030" "/etc/hadoop/conf/yarn-site.xml"
+sed -i s/localhost/$NAMENODE/ /etc/hadoop/conf/core-site.xml
+sed -i s/localhost/$RESOURCEMANAGER/ /etc/hadoop/conf/mapred-site.xml
+add_element "yarn.resourcemanager.hostname" "$RESOURCEMANAGER" "/etc/hadoop/conf/yarn-site.xml"
+add_element "yarn.resourcemanager.address" "$RESOURCEMANAGER:8032" "/etc/hadoop/conf/yarn-site.xml"
+add_element "yarn.resourcemanager.resource-tracker.address" "$RESOURCEMANAGER:8031" "/etc/hadoop/conf/yarn-site.xml"
+add_element "yarn.resourcemanager.scheduler.address" "$RESOURCEMANAGER:8030" "/etc/hadoop/conf/yarn-site.xml"
 add_element "dfs.namenode.datanode.registration.ip-hostname-check" "false" "/etc/hadoop/conf/hdfs-site.xml"
 
