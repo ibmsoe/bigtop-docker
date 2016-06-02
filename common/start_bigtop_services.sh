4c658e819fb78aadde2070666daadd0f3a3afc9e
amir sanjar <afsanjar@gmail.com>
2016-06-02 14:13:16 +0100

    adding wait for HDFS feature

--- common/start_bigtop_services.sh
+++ common/start_bigtop_services.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+wait_for_hdfs() {
+   >&2 echo "HDFS is unavailable"
+  # while su hdfs -c "hdfs dfsadmin -safemode wait" | grep ON
+   until nc -z namenode 8020
+      do
+       >&2 echo "..... waiting for HDFS"
+       sleep 5s 
+      done
+   >&2 echo "HDFS is available"
+}
 
 service ssh start
 ### Must run docker --privileged mode
@@ -8,6 +18,7 @@ echo -e  $node_ip_hostname >> /data/hosts
 umount /etc/hosts
 mv /etc/hosts /etc/hosts.bak
 ln -s /data/hosts /etc/hosts
+rm $HADOOP_CONF_DIR/slaves
 
 ## Do any optimization here
 if [ -f /data/pre_start.sh ]; then
@@ -21,8 +32,9 @@ do
           echo -e "127.0.0.1	localhost"> /data/hosts
           echo -e $node_ip_hostname>> /data/hosts
           service hadoop-hdfs-namenode restart
-          ## wait for namenode service 
-          sleep 10
+          ## wait for hdfs service 
+          sleep 20
+          wait_for_hdfs
           su hdfs -c "hdfs dfs -mkdir -p /user/root"
           su hdfs -c "hdfs dfs -chown root /user/root" 
           su hdfs -c "hdfs dfs -mkdir -p /tmp/hadoop-yarn/staging/history/done_intermediate" 
@@ -49,6 +61,7 @@ do
           service hadoop-yarn-timelineserver restart
 	;;
         "spark-master")
+          wait_for_hdfs
           service spark-master restart
           su hdfs -c "hadoop fs -mkdir -p /directory"
 	  su hdfs -c "hadoop fs -chown -R spark:hadoop /directory"
@@ -61,11 +74,13 @@ do
           service spark-worker restart
 	;;
         "zeppelin")
+          wait_for_hdfs
           su hdfs -c "hdfs dfs -mkdir /user/zeppelin"
           su hdfs -c "hdfs dfs -chown -R zeppelin /user/zeppelin"
           service zeppelin restart
 	;;
         "hive")
+          wait_for_hdfs
           su hdfs -c "hdfs dfs -mkdir -p /user/hive/warehouse"
           su hdfs -c "hdfs dfs -chown -R root /user/hive"
           /usr/lib/hive/bin/hiveserver2  &
